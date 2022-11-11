45164505f3228a8432bb2f73d3a7d9cac2c107da
SeongYum <dbal6930@gmail.com>
2022-11-11 14:26:05 +0900

    feat:물품 나눔 게시판 컴포넌트 추가

---
+++ client/src/Components/ShareList.js
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+function ShareList() {
+  return (
+    <ShareListContaner>
+      <ShareListTitle>
+        <Row1>
+          <h1>물품 나눔 게시판</h1>
+        </Row1>
+        <Row2>
+          <button>글쓰기</button>
+        </Row2>
+      </ShareListTitle>
+    </ShareListContaner>
+  );
+}
+
+export default ShareList;
+
+const ShareListContaner = styled.div`
+  width: 100%;
+  max-width: 1100px;
+`;
+
+const ShareListTitle = styled.div`
+  width: 100%;
+  max-width: 1100px;
+`;
+
+const Row1 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 50px;
+  margin: 0 0 12px;
+`;
+const Row2 = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 12px;
+`;
