cdcdbac9010cf257818acbafe98c934e87ca40b5
choizia0724 <drama_days@naver.com>
2022-11-11 17:19:22 +0900

    ⛲ feat :  메인화면 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react'; // eslint-disable-line no-unused-vars
-import Main from './Pages/Main';
+import Main from './pages/main';
+import Nav from './components/nav';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav />
         <Routes>
           <Route path="/" element={<Main />}></Route>
         </Routes>
