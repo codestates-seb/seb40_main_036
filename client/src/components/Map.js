1706fb34a3a97ea6b081e824c799bb40d86a8acb
choizia <st.jyeon@gmail.com>
2022-11-14 23:33:29 +0900

    ⛲ feat : 지도 sk로 변경

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -3,18 +3,17 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
 
-const { kakao } = window; // eslint-disable-line no-unused-vars
-
 const Map = (props) => {
   useEffect(() => {
-    let container = document.getElementById('map');
-
+    //let container = document.getElementById('map_div');
+    let container = 'map_div';
     let options = {
-      center: new window.kakao.maps.LatLng(props.x, props.y),
-      level: 3,
+      // 지도가 생성될 div
+      center: new window.Tmapv2.LatLng(props.x, props.y),
+      zoom: 16, // 지도 줌레벨
     };
     const Mapping = async () => {
-      await new window.kakao.maps.Map(container, options);
+      await new window.Tmapv2.Map(container, options);
       props.setloading(false);
     };
     Mapping();
@@ -29,7 +28,7 @@ const Map = (props) => {
         pulse={true}
         style={props.loading ? { display: 'block' } : { display: 'none' }}
       />
-      <MapWrapper id="map"></MapWrapper>
+      <MapWrapper id="map_div"></MapWrapper>
     </>
   );
 };
