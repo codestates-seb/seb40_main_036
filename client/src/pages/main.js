ceee1c1edb1f9a4f380efc21be01f6318ab7611b
choizia0724 <drama_days@naver.com>
2022-11-14 12:42:15 +0900

    ⛲ feat : js->react로 변환

--- client/src/pages/main.js
+++ client/src/pages/main.js
@@ -1,38 +1,60 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Button from '../components/button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { solid } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
 
 const Main = () => {
+  const [myLocation, setMyLocation] = useState(
+    37.566481622437934,
+    126.98502302169841
+  );
   useEffect(() => {
-    const script = document.createElement('script');
-    script.innerHTML = `         
-            function initTmap() {
-                var map = new Tmapv3.Map("TMapApp", {
-                    center: new Tmapv3.LatLng(37.566481622437934,126.98502302169841),
-                    width: "100%",
-                    height: "100%",
-                    zoom:15
-                });
-            }
-            
-            initTmap();
-       `;
-    script.type = 'text/javascript';
-    script.async = 'async';
-    document.head.appendChild(script);
+    initTmap();
   }, []);
+  useEffect(() => {
+    console.log(myLocation);
+  }, [myLocation]);
+  function initTmap() {
+    new window.Tmapv2.Map('TMapApp', {
+      center: new window.Tmapv2.LatLng(37.566481622437934, 126.98502302169841),
+      width: '100%',
+      height: '100%',
+      zoom: 15,
+    });
+  }
+
   const onclickhandle = () => {
     console.log('test');
   };
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      // GPS를 지원하면
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          setMyLocation(position.coords.latitude, position.coords.longitude);
+        },
+        function (error) {
+          console.error(error);
+        },
+        {
+          enableHighAccuracy: false,
+          maximumAge: 0,
+          timeout: Infinity,
+        }
+      );
+    } else {
+      alert('GPS를 지원하지 않습니다');
+    }
+  };
+
   return (
     <>
       <MapWrapper id="TMapApp" />
       <Positionrelative>
         <Button
-          onclick={onclickhandle}
           text={<FontAwesomeIcon icon={solid('location-crosshairs')} />}
+          onclick={getLocation}
         />
         <Button onclick={onclickhandle} text={'가장 가까운 대피소찾기'} />
       </Positionrelative>
@@ -58,7 +80,11 @@ const Positionrelative = styled.div`
     padding: 12px;
     border-radius: 50%;
     background: white;
-    border: 2px solid black;
+    border: 1px solid #008505;
+    color: #008505;
+  }
+  button:first-child:hover {
+    background: lightgray;
   }
   button:last-child {
     bottom: 40px;
@@ -69,7 +95,7 @@ const Positionrelative = styled.div`
     padding: 24px 36px;
     border-radius: 10px;
   }
-  button:hover {
+  button:last-child:hover {
     background: #005603;
   }
 `;
