6e911105c1653386d15e1c38a8cfd890626f18cf
choizia <st.jyeon@gmail.com>
2022-11-17 15:50:43 +0900

    ⚙ chore : 프록시 설정

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/user', '/question', '/answer'],
+    [
+      '/member',
+      '/shelter',
+      '/reservation',
+      '/reservationIfo',
+      '/question',
+      '/answer',
+      '/stuffQuestion',
+      '/stuffAnswer',
+    ],
     createProxyMiddleware({
       target: 'https://ef7d-211-214-195-126.jp.ngrok.io/',
       changeOrigin: true,
