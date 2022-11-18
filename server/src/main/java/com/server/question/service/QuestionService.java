f694137713dedf73005b2941e279c5af595ed1a9
lety <tkrhktyd@gmail.com>
2022-11-18 16:19:46 +0900

    refactor : sort questionId

--- server/src/main/java/com/server/question/service/QuestionService.java
+++ server/src/main/java/com/server/question/service/QuestionService.java
@@ -80,7 +80,7 @@ public class QuestionService {
 
     public Page<Question> findQuestions(int page, int size){
         return questionRepository.findAll(PageRequest.of(page,size,
-                Sort.by("memberId").descending()));
+                Sort.by("questionId").descending()));
     }
 
     public void deleteQuestion(long questionId){
