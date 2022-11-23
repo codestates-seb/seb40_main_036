6f79f6060682dec1eec5e2c8e503895bec69d52c
choizia <st.jyeon@gmail.com>
2022-11-23 14:29:43 +0900

    ⛲ feat : mypage생성

---
+++ client/src/pages/MyPage.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const MyPage = () => {
+  return (
+    <>
+      <MypageWrapper>Mypage</MypageWrapper>
+    </>
+  );
+};
+export default MyPage;
+
+const MypageWrapper = styled.div``;
