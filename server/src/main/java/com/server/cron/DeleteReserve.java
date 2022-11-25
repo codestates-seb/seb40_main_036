6a72b5297cf67d88c97835749b274da7623023b8
kyh0113 <cin05189@gmail.com>
2022-11-25 15:01:48 +0900

    cron 수정

--- server/src/main/java/com/server/cron/DeleteReserve.java
+++ server/src/main/java/com/server/cron/DeleteReserve.java
@@ -1,6 +1,7 @@
 package com.server.cron;
 
 import com.server.reservation.repository.ReservationRepository;
+import com.server.reservationInfo.entity.ReservationInfo;
 import com.server.reservationInfo.repository.ReservationInfoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootApplication
 @EnableScheduling
@@ -18,10 +19,15 @@ public class DeleteReserve {
     private final ReservationRepository reservationRepository;
     private final ReservationInfoRepository reservationInfoRepository;
 
-        @Scheduled(cron = "0 0 0 * * *")
+        // @Scheduled(cron="*/200 * * * * *") // 매일매일 100초 뒤에 아래의 코드를 실행하겠다.
+
+        @Scheduled(cron = "0 0 0 * * *") // 매일매일 0시에 아래의 코드를 실행하겠다.
         public void run(){
         reservationRepository.deleteAll();
-        reservationInfoRepository.deleteAll();
+            List<ReservationInfo> reservationInfoList=reservationInfoRepository.findAll();
+            for(ReservationInfo reservationInfo:reservationInfoList){
+                reservationInfo.setReservedNum(0); // 지우지말고 0으로 set
+            }
             System.out.println("작동");
     }
 }
