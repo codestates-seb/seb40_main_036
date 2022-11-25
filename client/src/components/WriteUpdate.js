1eb9e9df2719984472ad32b178ccb789c3581c0a
GopOwl <viogarden16@gmail.com>
2022-11-25 16:34:54 +0900

    feat : 글 수정 틀만 만들음 id 값을 못받아옴

--- client/src/components/WriteUpdate.js
+++ client/src/components/WriteUpdate.js
@@ -1,41 +1,96 @@
-import { useEffect, useState } from 'react';
-import styled from 'styled-components';
-import Editor from './Editor';
 import axios from 'axios';
+import { useState, useMemo } from 'react';
+import styled from 'styled-components';
+import DropDown from './Dropdown';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 import { useParams } from 'react-router-dom';
 
-const WriteUpdate = () => {
+const WriteForm = () => {
+  const modules = useMemo(
+    () => ({
+      toolbar: [
+        ['bold', 'italic', 'underline'],
+        [{ list: 'ordered' }, { list: 'bullet' }],
+        [{ size: ['small', false, 'large', 'huge'] }],
+        ['link', 'video'],
+        [{ color: [] }, { background: [] }],
+      ],
+    }),
+    []
+  );
+
+  const formats = [
+    'bold',
+    'italic',
+    'underline',
+    'list',
+    'size',
+    'link',
+    'image',
+    'video',
+    'color',
+    'background',
+  ];
+
+  // 초기값 - 제목, 내용
   const [title, setTitle] = useState('');
-  const [contents, setContents] = useState('');
+  const [contents, setContents] = useState(''); // eslint-disable-line no-unused-vars
+
+  // 지역 선택 드롭다운
+  const [drop, setDrop] = useState('');
 
   const { questionId } = useParams();
-
+  console.log(questionId);
   const onChangeTitle = (e) => {
     setTitle(e.target.value);
   };
 
+  // eslint-disable-next-line no-unused-vars
   const onChangeContents = (el) => {
     setContents(el);
   };
 
-  useEffect(() => {
-    const getBoard = async () => {
-      const { data } = await axios.get(`/question/${questionId}`);
-      return data;
-    };
-    getBoard().then((result) => {
-      setTitle(result.questionTitle);
-      setContents(result.questionContent);
-    });
-  }, []);
+  // const extractTextPattern = /(<([^>]+)>)/gi;
+  // let a = contents.replace(extractTextPattern, '');
+  // console.log(a);
 
-  // const canSubmit = useCallback(() => {
-  //   return contents !== '' && title !== '';
-  // }, [title, contents]);
+  // 지역 구 받아오는 값
+  const handleDrop = (e) => {
+    setDrop(e.target.value);
+  };
+
+  // const navigate = useNavigate();
+
+  const update = () => {
+    if (drop === '') {
+      return alert('지역을 선택하세요');
+    } else if (title === '') {
+      return alert('제목을 입력하세요');
+    } else if (contents === '') {
+      return alert('내용을 입력하세요');
+    }
+    axios
+      .patch(`/question/1`, {
+        memberId: sessionStorage.getItem('memberId'),
+        questionTitle: title,
+        questionContent: contents,
+        locationTag: drop,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        // Handle error.
+        console.log('An error occurred:', error);
+      });
+  };
 
   return (
-    <WriteUpdateStyle>
-      <form>
+    <WriteFormStyle>
+      <form className="input">
+        <div className="region">지역선택</div>
+        <DropDown onChange={handleDrop} value={drop} />
         <div className="title">
           <label className="titleText" htmlFor="titleWrite">
             제목
@@ -52,30 +107,45 @@ const WriteUpdate = () => {
         </div>
         <div>
           <div className="content">내용</div>
-          <Editor onchange={onChangeContents} value={contents} />
+          <EditorStyle>
+            <ReactQuill
+              className="editor"
+              modules={modules}
+              formats={formats}
+              value={contents}
+              onChange={onChangeContents}
+            />
+          </EditorStyle>
         </div>
       </form>
       <div className="reCancelBox">
-        <button className="registBox">
+        <button onClick={update} className="registBox">
           <div className="registInput">수정</div>
         </button>
         <button className="cancelBox">
           <div className="cancelInput">취소</div>
         </button>
       </div>
-    </WriteUpdateStyle>
+    </WriteFormStyle>
   );
 };
 
-export default WriteUpdate;
+export default WriteForm;
 
-const WriteUpdateStyle = styled.div`
+const WriteFormStyle = styled.div`
   width: 1180px;
   height: 100%;
   background: #ffffff;
   box-shadow: 4px 4px 10px 4px rgba(0, 0, 0, 0.25);
   margin: 50px auto;
 
+  .region {
+    padding-left: 100px;
+    padding-top: 60px;
+    padding-bottom: 12px;
+    font-weight: 400;
+    font-size: 24px;
+  }
   .title {
     padding: 50px;
   }
@@ -145,5 +215,16 @@ const WriteUpdateStyle = styled.div`
   .cancelInput {
     font-weight: 600;
     font-size: 20px;
+  }
+`;
+
+const EditorStyle = styled.div`
+  .editor {
+    text-align: center;
+    width: 983px;
+    height: 250px;
+    top: 495px;
+    margin-left: auto;
+    margin-right: auto;
   }
 `;
