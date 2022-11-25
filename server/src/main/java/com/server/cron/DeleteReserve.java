24e58f5c93c2cf4c4691489daf3060a99d40546e
kyh0113 <cin05189@gmail.com>
2022-11-26 01:07:04 +0900

    refactor:1시간마다 신데렐라

--- server/src/main/java/com/server/cron/DeleteReserve.java
+++ server/src/main/java/com/server/cron/DeleteReserve.java
@@ -19,9 +19,10 @@ public class DeleteReserve {
     private final ReservationRepository reservationRepository;
     private final ReservationInfoRepository reservationInfoRepository;
 
-        // @Scheduled(cron="*/200 * * * * *") // 매일매일 100초 뒤에 아래의 코드를 실행하겠다.
-
-        @Scheduled(cron = "0 0 0 * * *") // 매일매일 0시에 아래의 코드를 실행하겠다.
+        // @Scheduled(cron = "*/200 * * * * *") // 매일매일 100초 뒤에 아래의 코드를 실행하겠다.
+        // @Scheduled(cron = "0 0 0/1 * * *") // 매일매일 1시간 마다 아래의 코드를 실행하겠다.
+        // @Scheduled(cron = "0 0 0 * * *") // 매일매일 0시에 아래의 코드를 실행하겠다.
+        @Scheduled(cron = "0 0 0/1 * * *")
         public void run(){
         reservationRepository.deleteAll();
             List<ReservationInfo> reservationInfoList=reservationInfoRepository.findAll();
