9aeca073b192f39ffa5480dd7cf210b0d95b1c39
kyh0113 <cin05189@gmail.com>
2022-11-29 21:38:57 +0900

    refactor:message 띄우기

--- server/src/main/java/com/server/member/dto/MemberResponseDto.java
+++ server/src/main/java/com/server/member/dto/MemberResponseDto.java
@@ -1,12 +1,14 @@
 package com.server.member.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Builder
 public class MemberResponseDto {
     private long memberId;
 
