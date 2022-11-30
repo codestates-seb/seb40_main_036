5b4c1a4692862c1abfd88d94b8b4170972962060
lety <tkrhktyd@gmail.com>
2022-11-30 11:56:42 +0900

    memberId로 resevation 조회기능 추가

--- server/src/main/generated/com/server/reservationInfo/mapper/ReservationInfoMapperImpl.java
+++ server/src/main/generated/com/server/reservationInfo/mapper/ReservationInfoMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-30T02:03:56+0900",
+    date = "2022-11-30T11:27:18+0900",
     comments = "version: 1.5.1.Final, compiler: javac, environment: Java 17.0.5 (Amazon.com Inc.)"
 )
 @Component
