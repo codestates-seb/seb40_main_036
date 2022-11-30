bb2335f726034c30bef4a91633daa6792d8fb458
choizia <st.jyeon@gmail.com>
2022-11-30 20:25:21 +0900

    🚨 fix : 버그픽스

--- client/src/pages/MyPage.js
+++ client/src/pages/MyPage.js
@@ -31,13 +31,14 @@ const MyPage = () => {
       .get('/api/shelter/' + shelterId)
       .then((res) => setShelter(res.data.data));
   };
-  if (localStorage.getItem('email') !== null) {
-    setIsLogin(true);
-    setMemberId(localStorage.getItem('memberId'));
-    setUserName(localStorage.getItem('name'));
-    setUserEmail(localStorage.getItem('email'));
-  }
+
   useEffect(() => {
+    if (localStorage.getItem('email') !== null) {
+      setIsLogin(true);
+      setMemberId(localStorage.getItem('memberId'));
+      setUserName(localStorage.getItem('name'));
+      setUserEmail(localStorage.getItem('email'));
+    }
     fetchUser();
     !isLogin &&
       Swal.fire({
